4e168c389d48c3b252d2e00ca8ce84c648f6b551
wiryawanikadek <83597512+wiryawanikadek@users.noreply.github.com>
2023-06-11 22:50:09 +0800

    update2

--- library/List.java
+++ library/List.java
@@ -3,9 +3,9 @@ abstract class List {
     abstract void addMember(Member member);
     abstract Boolean isBookIdExist(String id);
     abstract Boolean isMemberIdExist(String id);
-    abstract void giveBook(String bookId, String memberId);
+    abstract void giveBook(String bookId, String memberId);    // Overloaded method
     abstract void receiveBook(String bookId, String memberId);
-     // Overloaded method
     abstract Book getBookById(String id);
   }
+  
   
